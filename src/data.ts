feb9d8d3bf53b6f01b32d5ab25565f0aa1271886
mariadm0414-spec <mariadm0414@gmail.com>
2026-03-09 19:15:51 -0500

    Delete src directory

--- src/data.ts
+++
@@ -1,5 +0,0 @@
-// This file re-exports from the main recipe data so it is compatible
-// with any legacy import of src/data.ts. All types match src/types.ts exactly.
-
-export { recipes as allRecipes, breakfasts, lunches, dinners } from './data/recipes';
-export { getMealPlan } from './data/mealPlan';
